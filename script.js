64f32397948d6234f4cfb42aba4f09101345e7e6
MitchellKloak <168693125+MitchellKloak@users.noreply.github.com>
2024-05-02 11:46:47 +0930

    making API work
    the API and some of my code wasn't working so i fixed it and updated the API's code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log("hello123");
 $(document).ready(function() {
     $("#searchBtn").on("click", function() {
         var weight = $("#WeightInput").val().trim();
@@ -18,56 +19,59 @@ $(document).ready(function() {
 });
 
 function getBMRData(weight, gender, height, age) {
-    const settings = {
-        async: true,
-        crossDomain: true,
-        url: `https://gym-fit.p.rapidapi.com/calculator/bmr?gender=${gender}&height=${height}&weight=${weight}&age=${age}`,
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
-            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+    console.log(weight, gender, height, age)
+const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://gym-fit.p.rapidapi.com/v1/calculator/bmr?weight=78&height=180&gender=male&age=25',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+        'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
         }
     };
 
     $.ajax(settings).done(function(response) {  
       console.log(response);
-        $("#BMRresultContainer").html(`<p>Your BMR (Basal metabolic rate) is: ${response}</p>`);
+        $("#BMRresultContainer").html(`<p>Your BMR (Basal metabolic rate) is: ${response.result}</p>`);
     })
 }
 
 
 function getIBWata(weight, gender, height, age) {
     const settings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://gym-fit.p.rapidapi.com/calculator/ibw?height=180',
-        headers: {
-            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
-            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
-        }
-    };
+    async: true,
+    crossDomain: true,
+    url: 'https://gym-fit.p.rapidapi.com/v1/calculator/ibw?height=180',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+        'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+    }
+};
 
     $.ajax(settings).done(function(response) {  
       console.log(response);
-        $("#IBWresultContainer").html(`<p>Your IBW (Ideal Body Weight) is: ${response}</p>`);
+        $("#IBWresultContainer").html(`<p>Your IBW (Ideal Body Weight) is: ${response.result}</p>`);
     })
 }
 
 
 function getTDEEData(weight, gender, height, age) {
     const settings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://gym-fit.p.rapidapi.com/calculator/tdee?age=25&weight=78&activityLevel=active&gender=male&height=180',
-        headers: {
-            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
-            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
-        }
-    };
+    async: true,
+    crossDomain: true,
+    url: 'https://gym-fit.p.rapidapi.com/v1/calculator/tdee?activityLevel=active&gender=male&age=25&weight=78&height=180',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+        'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+    }
+};
 
     $.ajax(settings).done(function(response) {  
       console.log(response);
-        $("#TDEEresultContainer").html(`<p>Your TDEE (Total Daily Energy Expenditure) is: ${response}</p>`);
+        $("#TDEEresultContainer").html(`<p>Your TDEE (Total Daily Energy Expenditure) is: ${response.result}</p>`);
     })
 }
 
@@ -75,17 +79,18 @@ function getTDEEData(weight, gender, height, age) {
 
 function getBMIData(weight, gender, height, age) {
     const settings = {
-        async: true,
-        crossDomain: true,
-        url: 'https://gym-fit.p.rapidapi.com/calculator/bmi?weight=78&height=180',
-        headers: {
-            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
-            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
-        }
-    };
+    async: true,
+    crossDomain: true,
+    url: 'https://gym-fit.p.rapidapi.com/v1/calculator/bmi?weight=78&height=180',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+        'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+    }
+};
 
     $.ajax(settings).done(function(response) {  
       console.log(response);
-        $("#BMIresultContainer").html(`<p>Your BMI (Body mass index) is: ${response}</p>`);
+        $("#BMIresultContainer").html(`<p>Your BMI (Body mass index) is: ${response.result}</p>`);
     })
 }
