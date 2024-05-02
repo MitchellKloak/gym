5c5425479c80bf2300838a2050c77cc76e7d781e
MitchellKloak <168693125+MitchellKloak@users.noreply.github.com>
2024-05-02 10:33:45 +0930

    initial setup
    this code is what i previously made, it is a gym stats tracker and using an API can have the data entered in a form and the data can be displayed.

---
+++ script.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    $("#searchBtn").on("click", function() {
+        var weight = $("#WeightInput").val().trim();
+        var height = $("#HeightInput").val().trim();
+        var gender = $("#GenderInput").val().trim().toLowerCase();
+        var age = $("#AgeInput").val().trim();
+
+        if (!weight, !height, !gender, !age) {
+            alert("Please fill in all fields.");
+            return;
+        }
+
+        getBMRData(weight, gender, height, age);
+        getIBWata(weight,gender,height,age);
+        getTDEEData(weight,gender,height,age);
+        getBMIData(weight,gender,height,age);
+    });
+});
+
+function getBMRData(weight, gender, height, age) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: `https://gym-fit.p.rapidapi.com/calculator/bmr?gender=${gender}&height=${height}&weight=${weight}&age=${age}`,
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function(response) {  
+      console.log(response);
+        $("#BMRresultContainer").html(`<p>Your BMR (Basal metabolic rate) is: ${response}</p>`);
+    })
+}
+
+
+function getIBWata(weight, gender, height, age) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://gym-fit.p.rapidapi.com/calculator/ibw?height=180',
+        headers: {
+            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function(response) {  
+      console.log(response);
+        $("#IBWresultContainer").html(`<p>Your IBW (Ideal Body Weight) is: ${response}</p>`);
+    })
+}
+
+
+function getTDEEData(weight, gender, height, age) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://gym-fit.p.rapidapi.com/calculator/tdee?age=25&weight=78&activityLevel=active&gender=male&height=180',
+        headers: {
+            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function(response) {  
+      console.log(response);
+        $("#TDEEresultContainer").html(`<p>Your TDEE (Total Daily Energy Expenditure) is: ${response}</p>`);
+    })
+}
+
+
+
+function getBMIData(weight, gender, height, age) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://gym-fit.p.rapidapi.com/calculator/bmi?weight=78&height=180',
+        headers: {
+            'X-RapidAPI-Key': '484e1c4bc1msh955f45b8300c55ap157b61jsn001cc190da62',
+            'X-RapidAPI-Host': 'gym-fit.p.rapidapi.com'
+        }
+    };
+
+    $.ajax(settings).done(function(response) {  
+      console.log(response);
+        $("#BMIresultContainer").html(`<p>Your BMI (Body mass index) is: ${response}</p>`);
+    })
+}
